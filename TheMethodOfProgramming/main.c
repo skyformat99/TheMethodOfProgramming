69c68260f72188d5e20bc050ae8d45936b81a83c
panzhangbao <panzhangbao@126.com>
2017-09-06 14:00:03 +0800

    Initial Commit

---
+++ TheMethodOfProgramming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TheMethodOfProgramming
+//
+//  Created by slcf666 on 2017/9/6.
+//  Copyright © 2017年 panzhangbao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
